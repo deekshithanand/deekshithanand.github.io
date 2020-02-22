bb0d31cdf9156d2af0ef3919aa13f7d4c2795854
deekshithanand <deekshith27anand@gmail.com>
2020-02-22 16:20:32 +0530

    Updates

---
+++ precache-manifest.703b4b5572d2e526b99ad1949471df65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aab2b3926a3b3da879ca091966c94e9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85b9cd5ba39a02285540",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "e98edbabf84f49aee2f6",
+    "url": "/static/css/main.827fec12.chunk.css"
+  },
+  {
+    "revision": "85b9cd5ba39a02285540",
+    "url": "/static/js/2.7ce8c571.chunk.js"
+  },
+  {
+    "revision": "406a75cb6674705c8e7a9815d1f39c15",
+    "url": "/static/js/2.7ce8c571.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e98edbabf84f49aee2f6",
+    "url": "/static/js/main.c236fd22.chunk.js"
+  },
+  {
+    "revision": "c0b1705d247db31fc439",
+    "url": "/static/js/runtime-main.6eae0b3b.js"
+  },
+  {
+    "revision": "053e6d35971319456982a5bb556b2af5",
+    "url": "/static/media/im2.053e6d35.jpg"
+  }
+]);
